24b24107dc3a0781cbb13dea464fa52cab1cbf20
Jchs <1997.jn@gmail.com>
2019-05-16 15:46:04 -0300

    Adições ao povoamento do BD

--- database/script.sql
+++ database/script.sql
@@ -43,12 +43,37 @@ CREATE TABLE user_lists (
 );
 
 
-INSERT INTO users (name, email, birthday, sex, permission) VALUES ('alex', 'alexandro@gmail.com', '1986-07-11', 0, 'USER');
-INSERT INTO users (name, email, birthday, sex, permission) VALUES ('valdecio', 'vald@gmail.com', '1964-02-01', 0, 'USER');
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('alex', 'alexandro@gmail.com', '1986-07-11', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('valdecio', 'vald@gmail.com', '1964-02-01', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('ana maria', 'braga@gmail.com', '1904-01-10', 1, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('marilyn manson', '666@gmail.com', '1969-01-05', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('robert plant', 'rblz@gmail.com', '1948-08-20', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('janis joplin', 'jjpearl@gmail.com', '1943-01-19', 1, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('dolores mary', 'eeidiswcw@gmail.com', '1971-09-06', 1, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('jim morrison', 'tchangeling@gmail.com', '1943-12-08', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('steven morrisey', 'englangismine@gmail.com', '1959-05-22', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('priscilla novaes', 'pitty@gmail.com', '1977-10-07', 1, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('joe strummer', 'tclash@gmail.com', '1952-08-21', 0, 'USER'); 
+INSERT INTO users (name, email, birthday, sex, permission) VALUES ('david byrne', 'psychok@gmail.com', '1952-05-14', 0, 'USER'); 
+
 
 INSERT INTO products (name, description) VALUES ('Toddynho', 'Achocolatado delicioso');
 INSERT INTO products (name, description) VALUES ('Nescau Achocolatado', 'Toddynho, so que de outra marca');
 INSERT INTO products (name, description) VALUES ('Leite Moça', 'Um Leite Condensado famoso ai');
+INSERT INTO products (name, description) VALUES ('Leite', 'De vaca');
+INSERT INTO products (name, description) VALUES ('Margarina', 'Dizem que é boa no pão');
+INSERT INTO products (name, description) VALUES ('Manteiga', 'Dizem que é melhor que margarina');
+INSERT INTO products (name, description) VALUES ('Iogurte', 'Gelado é bom');
+INSERT INTO products (name, description) VALUES ('Queijo', 'Rima com vinho');
+INSERT INTO products (name, description) VALUES ('Cerveja', 'Ahhhhhhh');
+INSERT INTO products (name, description) VALUES ('Vinho', 'Rima com queijo');
+INSERT INTO products (name, description) VALUES ('Whiskey', 'Sem gelo porfavor');
+INSERT INTO products (name, description) VALUES ('Vodka', 'Bons valores nutritivos');
+INSERT INTO products (name, description) VALUES ('Cachaça', 'Uma ideia boa puxa outra');
+INSERT INTO products (name, description) VALUES ('Frango', 'Cortado em bandeja');
+INSERT INTO products (name, description) VALUES ('Alcátra', 'Filé bom de assar');
+INSERT INTO products (name, description) VALUES ('Corvina', 'Peixe de couro pra cozinhar');
+INSERT INTO products (name, description) VALUES ('Camarão', 'Pra assar, alho e óleo');
 
 INSERT INTO stores (name, map) VALUES ('Ferreira Costa', 
 '{
@@ -59,16 +84,50 @@ INSERT INTO stores (name, map) VALUES ('Ferreira Costa',
   ]
 }');
 
-INSERT INTO lists (name, list) VALUES ('Awesome List', '{ "list": ["1","3","2"] }');
-INSERT INTO lists (name, list) VALUES ('Not so Awesome List', '{ "list": ["2"] }');
-INSERT INTO lists (name, list) VALUES ('Cuazy List', '{ "list": ["1","3","2"] }');
-INSERT INTO lists (name, list) VALUES ('Dumb List', '{ "list": ["2"] }');
+INSERT INTO lists (name, list) VALUES ('Meio de semana', '{ "list": ["1","2","3","4","5","7","8"] }');
+INSERT INTO lists (name, list) VALUES ('Fim de semana', '{ "list": ["15","9","11","13","3"] }');
+INSERT INTO lists (name, list) VALUES ('Lista Boçal', '{ "list": ["16","10","8"] }');
+INSERT INTO lists (name, list) VALUES ('Lista 1', '{ "list": ["1","2","7","17"] }');
+INSERT INTO lists (name, list) VALUES ('Lista 2', '{ "list": ["4","8","2","3","16"] }');
+INSERT INTO lists (name, list) VALUES ('list tatooed in reverse', '{ "list": ["1","3","6","15"] }');
+INSERT INTO lists (name, list) VALUES ('lista melhor que stairway to heaven', '{ "list": ["17","16","15","4","2"] }');
+INSERT INTO lists (name, list) VALUES ('lista pedaço do meu coração', '{ "list": ["1","2","7","17"] }');
+INSERT INTO lists (name, list) VALUES ('lista que demora', '{ "list": ["1"] }');
+INSERT INTO lists (name, list) VALUES ('List for those who live uptown', '{ "list": ["17"] }');
+INSERT INTO lists (name, list) VALUES ('list for Johnny Marr', '{ "list": ["3","2","1","15","7","9"] }');
+INSERT INTO lists (name, list) VALUES ('lista equalizável', '{ "list": ["3","4","1","2","6","10","11"] }');
+INSERT INTO lists (name, list) VALUES ('list for when london finally drown', '{ "list": ["10","12","13","14","15","17","1"] }');
+INSERT INTO lists (name, list) VALUES ('list for after the house burn', '{ "list": ["11","5","6","7","8","9","10"] }');
 
 INSERT INTO product_category (store_id, product_id, category) VALUES (1, 1, 'Achocolatado');
 INSERT INTO product_category (store_id, product_id, category) VALUES (1, 2, 'Achocolatado');
 INSERT INTO product_category (store_id, product_id, category) VALUES (1, 3, 'Leite Condensado');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 4, 'laticínios');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 5, 'laticínios');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 6, 'laticínios');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 7, 'laticínios');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 8, 'laticínios');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 9, 'bebidas alcoolicas');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 10, 'bebidas alcoolicas');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 11, 'bebidas alcoolicas');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 12, 'bebidas alcoolicas');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 13, 'bebidas alcoolicas');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 14, 'carnes e peixes');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 15, 'carnes e peixes');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 16, 'carnes e peixes');
+INSERT INTO product_category (store_id, product_id, category) VALUES (1, 17, 'carnes e peixes');
 
 INSERT INTO user_lists (user_id, list_id, store_id) VALUES (1, 1, 1);
 INSERT INTO user_lists (user_id, list_id, store_id) VALUES (1, 2, 1);
-INSERT INTO user_lists (user_id, list_id, store_id) VALUES (2, 3, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (3, 3, 1);
 INSERT INTO user_lists (user_id, list_id, store_id) VALUES (2, 4, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (2, 5, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (4, 6, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (5, 7, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (6, 8, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (7, 9, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (8, 10, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (9, 11, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (10, 12, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (11, 13, 1);
+INSERT INTO user_lists (user_id, list_id, store_id) VALUES (12, 14, 1);
